a3322696d14da50c2e16fe84060c126c52debb45
Count D <admin@countd.eu>
2014-03-10 15:00:49 +0200

    Initial commit
    Added two files used to print a Hello, World message.

---
+++ HelloTester.java
@@ -0,0 +1,8 @@
+public class HelloTester {
+
+    public static void main(String[] agrv) {
+        HelloWorld hi = new HelloWorld();
+        hi.sayHi();
+    }
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    
+    public void sayHi() {
+        System.out.println("Hello, World!");
+    }
+
+}
